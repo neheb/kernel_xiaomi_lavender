ce90c40441a2c2785fae47e6d54f69047a6520fb
Peppe289 <gsperanza204@gmail.com>
2022-11-13 14:13:26 +0100

    drivers: led: fix indexation
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- drivers/leds/leds-qpnp-flash-v2.c
+++ drivers/leds/leds-qpnp-flash-v2.c
@@ -1391,9 +1391,10 @@ static void qpnp_flashlight_led_brightness_set(struct led_classdev *led_cdev,
 				pr_debug(" switch name[%d] = %s, snode name[%d] = %s\n", i, flashlight_data->switch_name[i], j, led->snode[j].cdev.name);
 				if(!strcmp(flashlight_data->switch_name[i], led->snode[j].cdev.name)) {
 					rc = qpnp_flash_led_switch_set(&led->snode[j], false);
-				if (rc < 0)
-					pr_err("Failed to set flash LED switch rc=%d\n", rc);
-					break;
+					if (rc < 0) {
+						pr_err("Failed to set flash LED switch rc=%d\n", rc);
+						break;
+					}
 				}
 			}
 
@@ -1411,9 +1412,10 @@ static void qpnp_flashlight_led_brightness_set(struct led_classdev *led_cdev,
 				pr_debug(" switch name[%d] = %s, snode name[%d] = %s\n", i, flashlight_data->switch_name[i], j, led->snode[j].cdev.name);
 				if(!strcmp(flashlight_data->switch_name[i], led->snode[j].cdev.name)) {
 					rc = qpnp_flash_led_switch_set(&led->snode[j], value > 0);
-				if (rc < 0)
-					pr_err("Failed to set flash LED switch rc=%d\n", rc);
-					break;
+					if (rc < 0) {
+						pr_err("Failed to set flash LED switch rc=%d\n", rc);
+						break;
+					}
 				}
 			}
 	}
@@ -1740,7 +1742,8 @@ static int qpnp_flash_led_parse_each_led_dt(struct qpnp_flash_led *led,
        #ifdef CONFIG_KERNEL_CUSTOM_F7A
 	if(fnode->type == FLASH_LED_TYPE_TORCH)
 	{
- 		  fnode->cdev.flags |= LED_KEEP_TRIGGER;
+
+ 		  fnode->cdev.flags |= LED_KEEP_TRIGGER;
 	}
 	#endif
 	rc = led_classdev_register(&led->pdev->dev, &fnode->cdev);
