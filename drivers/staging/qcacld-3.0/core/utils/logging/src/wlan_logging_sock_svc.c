eac68299661db5080e7af869726da6dc759bfd23
Peppe289 <gsperanza204@gmail.com>
2022-11-13 14:45:14 +0100

    drivers: qcacld-3.0: fix indexation
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- drivers/staging/qcacld-3.0/core/utils/logging/src/wlan_logging_sock_svc.c
+++ drivers/staging/qcacld-3.0/core/utils/logging/src/wlan_logging_sock_svc.c
@@ -597,19 +597,20 @@ int pktlog_send_per_pkt_stats_to_user(void)
 			ret = 0;
 		}
 err:
-	/*
-	 * Free old skb in case or error before assigning new skb
-	 * to the free list.
-	 */
-	if (free_old_skb)
-		dev_kfree_skb(pstats_msg->skb);
+		/*
+		* Free old skb in case or error before assigning new skb
+		* to the free list.
+		*/
+		if (free_old_skb) {
+			dev_kfree_skb(pstats_msg->skb);
 
-		spin_lock_irqsave(&gwlan_logging.pkt_stats_lock, flags);
-		pstats_msg->skb = skb_new;
-		list_add_tail(&pstats_msg->node,
-				&gwlan_logging.pkt_stat_free_list);
-		spin_unlock_irqrestore(&gwlan_logging.pkt_stats_lock, flags);
-		ret = 0;
+			spin_lock_irqsave(&gwlan_logging.pkt_stats_lock, flags);
+			pstats_msg->skb = skb_new;
+			list_add_tail(&pstats_msg->node,
+					&gwlan_logging.pkt_stat_free_list);
+			spin_unlock_irqrestore(&gwlan_logging.pkt_stats_lock, flags);
+			ret = 0;
+		}
 	}
 
 	return ret;
