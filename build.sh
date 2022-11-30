28d48a9989b02350d02e0b10f0d8cd99788363ea
Peppe289 <gsperanza204@gmail.com>
2022-11-30 20:15:19 +0100

    build: use EVA GCC
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- build.sh
+++ build.sh
@@ -58,59 +58,9 @@ export KBUILD_BUILD_HOST="RaveRules"
 DATE=$(date +"%Y%m%d-%H%M")
 
 #
-# Compiler type
-#
-TOOLCHAIN_DIRECTORY="../toolchain"
-TOOLCHAIN_ARM32="gcc-arm"
-TOOLCHAIN_ARM64="gcc-arm64"
-
-#
 # Build defconfig
 #
 DEFCONFIG="lavender-perf_defconfig"
-
-#
-# Check for compiler
-#
-if [ ! -d "$TOOLCHAIN_DIRECTORY" ]; then
-    mkdir $TOOLCHAIN_DIRECTORY
-fi
-
-if [ $COMPILER_TYPE == "GCC" ]; then
-
-    if [ -d "$TOOLCHAIN_DIRECTORY/$TOOLCHAIN_ARM32" ]; then
-        echo -e "${bldgrn}"
-        echo "Toolchain arm32 ready"
-        echo -e "${txtrst}"
-    else
-        echo -e "${red}"
-        echo "Need to download toolchain arm32"
-        echo -e "${txtrst}"
-        git clone --depth=1 https://github.com/Rave-Project/arm-linux-androideabi-4.9.git $TOOLCHAIN_DIRECTORY/$TOOLCHAIN_ARM32
-    fi
-
-    if [ -d "$TOOLCHAIN_DIRECTORY/$TOOLCHAIN_ARM64" ]; then
-        echo -e "${bldgrn}"
-        echo "Toolchain arm64 ready"
-        echo -e "${txtrst}"
-    else
-        echo -e "${red}"
-        echo "Need to download toolchain arm64"
-        echo -e "${txtrst}"
-        git clone --depth=1 https://github.com/Rave-Project/aarch64-linux-android-4.9.git $TOOLCHAIN_DIRECTORY/$TOOLCHAIN_ARM64
-    fi
-else
-    if [ -d "$TOOLCHAIN_DIRECTORY/clang" ]; then
-        echo -e "${bldgrn}"
-        echo "Proton-Clang is ready"
-        echo -e "${txtrst}"
-    else
-        echo -e "${red}"
-        echo "Need to download Proton-Clang"
-        echo -e "${txtrst}"
-        git clone --depth=1 https://github.com/Peppe289/proton-clang.git $TOOLCHAIN_DIRECTORY/clang
-    fi
-fi
 
 if [ $SEND ]; then
     curl -s -X POST https://api.telegram.org/bot"${1}"/sendMessage \
@@ -120,35 +70,13 @@ if [ $SEND ]; then
         -d text="<b>• Build For Lavender started •</b>"
 fi
 
-if [ $COMPILER_TYPE == "GCC" ]; then
-    #
-    # Build start with GCC
-    #
-    export CROSS_COMPILE=$(pwd)/$TOOLCHAIN_DIRECTORY/$TOOLCHAIN_ARM64/bin/aarch64-linux-androidkernel-
-    export CROSS_COMPILE_ARM32=$(pwd)/$TOOLCHAIN_DIRECTORY/$TOOLCHAIN_ARM32/bin/arm-linux-androideabi-
+export CROSS_COMPILE=$(pwd)/../gcc-arm64/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=$(pwd)/../gcc-arm/bin/arm-eabi-
 
-    export ARCH=arm64
-    export SUBARCH=arm64
+export ARCH=arm64 && export SUBARCH=arm64
 
-    make O=out $DEFCONFIG
-    make O=out -j$(nproc --all)
-else
-    #
-    # Build start with Proton Clang
-    #
-    PATH="$(pwd)/$TOOLCHAIN_DIRECTORY/clang/bin:${PATH}"
-    make O=out ARCH=arm64 $DEFCONFIG
-    make -j$(nproc --all) O=out \
-				ARCH=arm64 \
-				CC=clang \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				OBJCOPY=llvm-objcopy \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip \
-				CROSS_COMPILE=aarch64-linux-gnu- \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-fi
+make O=out ARCH=arm64 $DEFCONFIG
+make -j$CPU O=out
 
 if [ ! -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
     echo -e "${red}"
